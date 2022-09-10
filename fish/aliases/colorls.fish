142ce07e477bc82850d61629eb406af73ba6d019
hiroshi-tarumi <tarumi8171@gmail.com>
2022-09-10 13:01:30 +0900

    test

---
+++ fish/aliases/colorls.fish
@@ -0,0 +1,13 @@
+alias l="colorls -a"
+alias la="colorls -la"
+alias lad="colorls -la --sd"
+alias lar="colorls -la --report"
+alias ld="colorls -a --sd"
+alias ldr="colorls -a --sd --report"
+alias lf="colorls -a --sf"
+alias laf="colorls -la --sf"
+alias ll="colorls *"
+alias lla="colorls * -la --sd"
+alias llr="colorls * -la --sd --report"
+alias lr="colorls -a --report"
+
